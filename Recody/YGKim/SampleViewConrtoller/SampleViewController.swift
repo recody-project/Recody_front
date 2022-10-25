8ba04f96657a74ef33aa117c0aef669185fe5db9
GKim <40786028+kookooKim@users.noreply.github.com>
2022-10-25 17:45:38 +0900

    YGKim
    CleanArchitecture 추상화 완료
    적용 샘플예제는 다음 커밋에 하겠습니다. 아직 사용 x

---
+++ Recody/YGKim/SampleViewConrtoller/SampleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SampleViewController.swift
+//  Recody
+//
+//  Created by Glory Kim on 2022/10/25.
+//
+
+import Foundation
+import UIKit
+
+class SampleViewController : CommonVC,DataPassingType{
+    
+    func bind(_ data: DataStoreType) {
+        if let bindingData = data.data(SomeOrder.new)?.fetch(ChildDataModel.self) {
+            
+        }else{
+            
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
